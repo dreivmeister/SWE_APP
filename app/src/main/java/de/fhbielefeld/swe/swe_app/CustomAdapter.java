1fb07ef63adb6731cc1cb386d973657b754a74ed
dreivmeister <93375746+dreivmeister@users.noreply.github.com>
2021-12-09 22:54:03 +0100

    Filter hinzugefügt

---
+++ app/src/main/java/de/fhbielefeld/swe/swe_app/CustomAdapter.java
@@ -0,0 +1,65 @@
+package de.fhbielefeld.swe.swe_app;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+//SOURCE: https://stackoverflow.com/questions/14663725/list-view-filter-android
+
+public class CustomAdapter extends BaseAdapter implements Filterable {
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public Filter getFilter() {
+        Filter filter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults results = new FilterResults();
+                ArrayList<Integer> FilteredRaumNummern = new ArrayList<>();
+
+                //tatsächliches filtern
+                constraint = constraint.toString().toLowerCase();
+                for(int i = 0; i < MainActivity.getRaumListe().size(); i++) {
+                    int RaumNummer = MainActivity.getRaumListe().get(i).raumnummer;
+
+                    if (RaumNummer == Integer.parseInt(constraint.toString())) {
+                        FilteredRaumNummern.add(RaumNummer);
+                    }
+                }
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                arrayListNames = (List<Integer>) results.values;
+                notifyDataSetChanged();
+            }
+        };
+        return filter;
+    }
+}
--- app/src/main/java/de/fhbielefeld/swe/swe_app/MainActivity.java
+++ app/src/main/java/de/fhbielefeld/swe/swe_app/MainActivity.java
@@ -5,15 +5,18 @@ import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.List;
 
 public class MainActivity<adapter> extends AppCompatActivity {
-
+    static List<Raum> raumListe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,15 +50,34 @@ public class MainActivity<adapter> extends AppCompatActivity {
         //kann jetzt mit den Funktionen in RaumDao verwendet werden
 
         //erstellen der raumListe für die Darstellung in MainActivity
-        List<Raum> raumListe = raumDao.getAll();
+        raumListe = raumDao.getAll();
 
         //verknüpfung von ListView und raumListe
         ArrayAdapter<Raum> adapter = new ArrayAdapter<Raum>(this, android.R.layout.simple_list_item_1, raumListe);
         ListView listView = (ListView) findViewById(R.id.Ergebnisse);
         listView.setAdapter(adapter);
 
+        EditText inputSearch = (EditText) findViewById(R.id.Suche);
+        //i dont know if it belongs here
+        inputSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                adapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+        
+
+
+
     }
 
-
-
+    static List<Raum> getRaumListe() {
+        return raumListe;
+    }
 }
